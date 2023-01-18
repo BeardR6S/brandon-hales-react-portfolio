c8f515802a5f48319e30af1e71edfcfd907d6177
BeardR6S <brandon.allan.hale@gmail.com>
2023-01-18 03:34:03 -0500

    nav bar updates and other things

--- src/components/app.js
+++ src/components/app.js
@@ -1,15 +1,32 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import moment from "moment";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import PortfolioContainer from "./portfolio/portfolio-container";
 import NavigationContainer from "./navigation/navigation-container";
+import Home from "./pages/home";
+import About from "./pages/about";
+import Contact from "./pages/contact";
+import Blog from "./pages/blog"
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-      <NavigationContainer />
+        <Router>
+          <div>
+            <NavigationContainer />
+
+            <Switch>
+              <Route exact path='/' component={Home} />
+              <Route path='/about-me' component={About} />
+              <Route path='/contact' component={Contact} />
+              <Route path='/blog' component={Blog} />
+            </Switch>
+          </div>
+        </Router>
+
         <h1>Brandon Hales Portfolio</h1>
-        <div>{moment().format('MMMM Do YYYY, h:mm:ss a')}</div>
+        <div>{moment().format("MMMM Do YYYY, h:mm:ss a")}</div>
         {/* adds a month/day/year/hour/minute/second/AM/PM */}
         <PortfolioContainer />
       </div>
