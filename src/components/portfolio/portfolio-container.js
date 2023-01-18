f4aa4f0eaae9aba8ab93a3241e793a876ec79158
BeardR6S <brandon.allan.hale@gmail.com>
2023-01-18 02:36:46 -0500

    Multiple Changes

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -1,21 +1,35 @@
 import React, { Component } from "react";
-import PortfolioItem from "./portfolio-item";
 
-import portfolioItem from "./portfolio-item";
+import PortfolioItem from "./portfolio-item";
 
 export default class PortfolioContainer extends Component {
     constructor() {
         super();
 
         this.state = {
-            pageTitle: "Welcome to my portfolio", 
+            pageTitle: "Welcome to my portfolio",
+            isLoading: false,
             data: [
-                {title: "United States Army" },
-                {title: "Continental Building Products" },
-                {title: "T-Mobile" },
-                {title: "Amazon" }
+                {title: "United States Army", category: "Government" },
+                {title: "Continental Building Products", category: "Manufacturing" },
+                {title: "Anderson Manufacturing and Rifles", category: "Government"},
+                {title: "L3 Harris Technologies", category: "Government"},
+                {title: "Bosch", category: "Inventory Management"},
+                {title: "Amazon", category: "Inventory Management" }
             ]
-        }
+        };
+
+        this.handleFilter = this.handleFilter.bind(this);
+    }
+
+    
+
+    handleFilter(filter) {
+        this.setState({
+            data: this.state.data.filter(item => {
+                return item.category === filter;
+            })
+        })
     }
 
     portfolioItems() {
@@ -25,9 +39,23 @@ export default class PortfolioContainer extends Component {
     }
 
     render() {
+        if (this.state.isLoading) {
+            return <div>Loading...</div>;
+        }
+
         return (
             <div>
                 <h2>{this.state.pageTitle}</h2>
+
+                <button onClick={() => this.handleFilter('Government')}>
+                Government
+                </button>
+                <button onClick={() => this.handleFilter("Manufacturing")}>
+                Manufacturing
+                </button>
+                <button onClick={() => this.handleFilter("Inventory Management")}>
+                Inventory Management
+                </button>
 
                 {this.portfolioItems()}
             </div>
