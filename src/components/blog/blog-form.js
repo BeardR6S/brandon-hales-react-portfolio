2a7e3b8383884fb4eb9b8fec7ec44c450e03ebac
BeardR6S <brandon.allan.hale@gmail.com>
2023-03-27 09:06:10 -0400

    Early Morning Update

--- src/components/blog/blog-form.js
+++ src/components/blog/blog-form.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
+import { DropzoneComponent } from "react-dropzone-component";
 
 import RichTextEditor from "../forms/rich-text-editor";
 
@@ -11,12 +12,40 @@ export default class BlogForm extends Component {
       title: "",
       blog_status: "",
       content: "",
+      featured_image: "",
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleRichTextEditorChange =
       this.handleRichTextEditorChange.bind(this);
+
+    // Below is for the blog modal dropzone image.
+    this.componentConfig = this.componentConfig.bind(this);
+    this.djsConfig = this.djsConfig.bind(this);
+    this.handleFeaturedImageDrop = this.handleFeaturedImageDrop.bind(this);
+
+    this.featuredImageRef = React.createRef();
+  }
+
+  componentConfig() {
+    return {
+      iconFiletypes: [".jpg", ".png"],
+      showFiletypeIcon: true,
+      postUrl: "https://httpbin.org/post",
+    };
+  }
+  djsConfig() {
+    return {
+      addRemoveLinks: true,
+      maxFiles: 1,
+    };
+  }
+
+  handleFeaturedImageDrop() {
+    return {
+      addedfile: (file) => this.setState({ featured_image: file }),
+    };
   }
 
   handleRichTextEditorChange(content) {
@@ -31,6 +60,13 @@ export default class BlogForm extends Component {
     formData.append("portfolio_blog[blog_status]", this.state.blog_status);
     formData.append("portfolio_blog[content]", this.state.content);
 
+    if (this.state.featured_image) {
+      formData.append(
+        "portfolio_blog[featured_image]",
+        this.state.featured_image
+      );
+    }
+
     return formData;
   }
 
@@ -43,6 +79,10 @@ export default class BlogForm extends Component {
       )
 
       .then((response) => {
+        if (this.state.featured_image) {
+          this.featuredImageRef.current.dropzone.removeAllFiles();
+        }
+
         this.setState({
           title: "",
           blog_status: "",
@@ -89,6 +129,18 @@ export default class BlogForm extends Component {
           <RichTextEditor
             handleRichTextEditorChange={this.handleRichTextEditorChange}
           />
+        </div>
+
+        <div className='image-uploader'>
+          <DropzoneComponent
+            ref={this.featuredImageRef}
+            // below add () to make it run right away.
+            config={this.componentConfig()}
+            djsConfig={this.djsConfig()}
+            eventHandlers={this.handleFeaturedImageDrop()}
+          >
+            <div className='dz-message'>Featured image</div>
+          </DropzoneComponent>
         </div>
 
         <button className='btn'>Save</button>
